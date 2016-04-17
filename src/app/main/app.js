428ef67719962d0f48b71b0363574ad5268807ee
Punkbooster <2morrow2late@rambler.ru>
2016-04-17 15:56:38 +0200

    Fetch seeds data from the controller to index

--- src/app/main/app.js
+++ src/app/main/app.js
@@ -11,30 +11,30 @@
       title: 'A Game of Thrones: A Song of Ice and Fire',
       author: 'George R.R. Martin',
       isbn: '0553593714',
-      review: 'no review',
+      review: 'This book was SO cool!',
       rating: 4,
       genres: ['non-fiction']
     },{
       title: 'HTML for Babies',
       author: 'John C Vanden-Heuvel Sr',
       isbn: '0615487661',
-      review: "no review",
+      review: "Love this book.",
       rating: 5,
       genres: ['non-fiction']
     },{
       title: 'A is for Array',
       author: 'Brandon J Hansen',
       isbn: '1489522212',
-      review: 'no review',
+      review: 'Extremely satisfied!',
       rating: 4,
       genres: ['non-fiction']
     },{
       title: 'The Dragon Reborn',
       author: 'Robert Jordan',
       isbn: '0812513711',
-      review: 'no review',
+      review: 'Worst book ever.',
       rating: 4,
-      genres: ['non-fiction']
+      genres: ['non-fiction', 'fantasy']
     }
   ];
 })();
